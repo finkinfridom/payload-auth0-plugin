55079e8bb5f56719387776669c5e0ffb6a9bbc49
Alberto Maghini <alberto@newesis.com>
2022-11-12 11:11:14 +0100

    fixed webpack

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,14 @@
-export * from "./Avatar";
-export * from "./LoginButton";
-export * from "./LogoutButton";
-export * from "./PictureField";
+import Avatar, { Auth0User } from "./Avatar";
+import LoginButton, { LoginButtonProps } from "./LoginButton";
+import LogoutButton, { LogoutButtonProps } from "./LogoutButton";
+import PictureField from "./PictureField";
+
+export {
+  Avatar,
+  LoginButton,
+  LoginButtonProps,
+  LogoutButton,
+  LogoutButtonProps,
+  PictureField,
+  Auth0User,
+};
---
+++ webpack.components.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const TerserJSPlugin = require("terser-webpack-plugin");
+
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: {
+    main: [path.resolve(__dirname, "dist/components/index.js")],
+  },
+  externals: {
+    react: "react",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/components"),
+    publicPath: "/",
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  mode: "production",
+  stats: "errors-only",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          {
+            test: /\.(sa|sc|c)ss$/,
+            sideEffects: true,
+            use: [
+              MiniCSSExtractPlugin.loader,
+              "css-loader",
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: ["postcss-preset-env"],
+                  },
+                },
+              },
+              "sass-loader",
+            ],
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: require.resolve("file-loader"),
+            options: {
+              emitFile: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    modules: ["node_modules"],
+  },
+};
